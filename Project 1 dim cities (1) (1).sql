1aa296270d4b8ed40af16866b664899bcb04f4aa
Neeraj4077 <neerajthakur14072002@gmail.com>
2025-09-19 19:40:44 +0530

    Add files via upload

---
+++ Project 1 dim cities (1) (1).sql
@@ -0,0 +1,153 @@
+create database project1;
+use project1;
+desc products;
+select count(*) from transactions_1997;
+select count(*) from transactions_1998;
+select count(*) from regions;
+select count(*) from customers;
+select count(*) from products;
+select * from regions;
+select * from stores;
+select * from returns_1997_1998;
+select * from transactions_1997; 
+select * from transactions_1998;
+select * from products;
+select * from customers;
+alter table transactions_1997
+add constraint tran_1997_fk
+foreign key (customer_id) references customers(customer_id);
+
+alter table transactions_1998 
+add constraint tran_1998_fk
+foreign key (customer_id) references customers (customer_id);
+
+alter table returns_1997_1998
+add constraint re_978_fk
+foreign key (product_id) references products(product_id);
+
+alter table transactions_1997
+add constraint t_19_fk
+foreign key (product_id) references products(product_id);
+
+alter table transactions_1998
+add constraint tra_98_fk
+foreign key (product_id) references products(product_id);
+
+alter table returns_1997_1998
+add constraint re_99_fk
+foreign key (store_id) references stores(store_id);
+
+alter table transactions_1997
+add constraint tion_97_fk
+foreign key (store_id) references stores(store_id);
+
+alter table transactions_1998
+add constraint tan_98_fk
+foreign key (store_id) references stores(store_id);
+
+alter table stores
+add constraint st_99_fk
+foreign key (region_id) references regions(region_id);
+
+alter table customers
+drop column customerscol;
+
+
+
+		 -- Question Solve
+
+-- Q1. Top 3 products having max revenue?
+
+SELECT p.product_brand, round(SUM(p.product_retail_price * t.quantity),2) 
+AS products_max_revenue   
+FROM products p 
+join transactions_1997 t ON p.product_id = t.product_id
+GROUP BY product_brand
+ORDER BY products_max_revenue  DESC limit 3;
+
+-- Q2. Top 3 profitable products?
+
+SELECT product_name,round(sum(product_retail_price - product_cost),2)
+AS Profittable_products 
+FROM products
+GROUP BY product_name
+ORDER BY Profittable_products DESC
+LIMIT 3;
+
+-- Q3. Top 3 customers who had spend max amount?
+
+SELECT c.customer_id, c.first_name, round(sum(p.product_retail_price * t.quantity),2)
+AS spend_max_amount
+FROM customers c
+JOIN transactions_1997 t ON c.customer_id = t.customer_id
+JOIN products p ON p.product_id = t.product_id
+GROUP BY c.customer_id, c.first_name
+ORDER BY spend_max_amount DESC
+LIMIT 3;
+
+
+-- Q4. Top 3 stores with max profit?
+
+SELECT store_name, round(sum((p.product_retail_price-p.product_cost)*t.quantity),2) 
+AS profit 
+FROM stores s
+JOIN transactions_1997 t ON t.store_id = s.store_id 
+JOIN products p ON p.product_id = t.product_id
+GROUP BY store_name
+ORDER BY  profit DESC
+LIMIT 3;
+
+-- Q5. Customers who never purchased anything?
+
+SELECT c.customer_id,c.first_name 
+FROM customers c
+LEFT JOIN transactions_1997 t ON c.customer_id = t.customer_id
+WHERE t.customer_id IS NULL;
+
+-- Q6. Customer who never purchased?
+SELECT c.customer_id,c.first_name 
+FROM customers c
+LEFT JOIN transactions_1997 t ON c.customer_id = t.customer_id
+WHERE t.customer_id IS NULL;
+
+-- Q7. Customer who purchased 3 months ago but not purchasing now?
+
+SELECT customer_id,transaction_date FROM transactions_1997 
+WHERE customer_id NOT IN
+(SELECT customer_id  
+FROM transactions_1997
+WHERE datediff("1997-12--31",transaction_date) <=90
+GROUP BY customer_id)
+GROUP BY customer_id,transaction_date
+ORDER BY transaction_date DESC;
+
+ 
+-- Q8. Most returned products (more than 10)?
+
+SELECT p.product_id, SUM(r.quantity) 
+AS total_returns
+FROM products p
+JOIN returns_1997_1998 r ON p.product_id = r.product_id
+GROUP BY p.product_id
+HAVING total_returns > 10
+ORDER BY total_returns DESC;
+
+
+-- Q9. Sales age group(18-30,31-50,>51)
+
+SELECT 
+CASE 
+WHEN 1997-birthdate BETWEEN 15 AND 30 THEN '18-30'
+WHEN 1997-birthdate BETWEEN 31 AND 50 THEN '31-50'
+WHEN 1997-birthdate > 50 THEN '>51'
+ELSE 'Unknown'
+END AS age_group,COUNT(*) AS total_sales,round(SUM(product_retail_price),2) AS total_sales_amount
+FROM customers s
+JOIN transactions_1997 t ON t.customer_id = s.customer_id 
+JOIN products p ON p.product_id = t.product_id
+GROUP BY age_group
+ORDER BY age_group;
+
+
+-- Q10. Most popular products among age groups?
+
